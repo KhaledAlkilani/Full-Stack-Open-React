d1c4454ef0ef46bc91ba379a0cfc06fe9d503666
Khaled <khaledkelany@gmail.com>
2022-04-07 12:47:29 +0300

    tasK1112

---
+++ React/Task 1.1-1.2/task/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './osa01/Task1.2/AppTask1.2';
+
+ReactDOM.render(<App/>, document.getElementById('root'));
---
+++ React/Task 1.1-1.2/task/src/osa01/Task1.2/Total.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Total = (props) => {
+    return(
+        <p style={{color: 'red'}}>{props.total}</p>
+    )
+}
+
+export default Total
