37193e2ecfd3dddc961382f2748e86bd103d07e8
Khaled <khaledkelany@gmail.com>
2022-04-11 18:12:13 +0300

    task1.5Index

--- fullstackopen/src/index.js
+++ fullstackopen/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './osa01/cHarjoitus/App';
+import App from './osa01/Task1.5/AppTask1.5';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 
