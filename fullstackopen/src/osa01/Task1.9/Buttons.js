4b53685ee56fe87f08a9248d63c64bf511fdbe49
Khaled <khaledkelany@gmail.com>
2022-04-14 16:32:54 +0300

    Task1.9Update

--- fullstackopen/src/osa01/Task1.9/Buttons.js
+++ fullstackopen/src/osa01/Task1.9/Buttons.js
@@ -14,24 +14,9 @@ const Buttons = (props) => {
         fontWeight: 'bold'}} 
         onClick={props.handleClick}>
         {props.text}
-</button>,
-
-<button style=
-        {{borderRadius: '10px', 
-        border: 'none', 
-        width: '70px', 
-        height: '1.6rem', 
-        marginRight: '10px',
-        color: 'red',
-        fontWeight: 'bold'}} 
-        onClick={props.handleClick}>
-        {props.text}
 </button>
 
 )
     }
-
-    
-    
 
 export default Buttons
